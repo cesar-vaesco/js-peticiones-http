52c8bea4532ab9ce04e551011319e4cb2e1f66ef
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-02-15 10:18:04 -0600

    http: creando elementos html de forma dinamica que mostrar los chistes

---
+++ src/js/chistes-page.js
@@ -0,0 +1,28 @@
+
+
+
+const body = document.body;
+
+
+const crearChistesHtml = () => {
+
+  const html = `<h1 class="mt-5">Chistes</h1>
+    <hr>
+    <button class="btn btn-primary"></button>
+
+    <ol class="mt-2 list-group">
+        <li class="list-group-item">....</li>
+    </ol>`;
+
+    const divChistes = document.createElement('div');
+
+    divChistes.innerHTML = html;
+
+    body.append(divChistes);
+}
+
+
+export const init = () =>{
+
+  crearChistesHtml();
+}
