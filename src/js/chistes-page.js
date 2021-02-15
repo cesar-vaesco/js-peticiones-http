8354026d483e6ae0e5d19869434422f0e71688f2
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-02-15 11:49:24 -0600

    -http: dando funcionalidad a botón - configurando evento

--- src/js/chistes-page.js
+++ src/js/chistes-page.js
@@ -3,26 +3,40 @@
 
 const body = document.body;
 
+let btnOtro, olListChistes;
+
 
 const crearChistesHtml = () => {
 
   const html = `<h1 class="mt-5">Chistes</h1>
     <hr>
-    <button class="btn btn-primary"></button>
+    <button class="btn btn-primary">Otro chiste!</button>
 
     <ol class="mt-2 list-group">
         <li class="list-group-item">....</li>
     </ol>`;
 
-    const divChistes = document.createElement('div');
+  const divChistes = document.createElement('div');
 
-    divChistes.innerHTML = html;
+  divChistes.innerHTML = html;
 
-    body.append(divChistes);
+  body.append(divChistes);
+}
+
+const eventos = () => {
+
+  olListChistes = document.querySelector('ol');
+  btnOtro = document.querySelector('button');
+
+  btnOtro.addEventListener('click', () =>{
+    console.log('Hola mundo...');
+  });
+
 }
 
 
-export const init = () =>{
+export const init = () => {
 
   crearChistesHtml();
+  eventos();
 }
