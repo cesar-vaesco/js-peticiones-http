aa33c87ef9da1d40f96cb3848a0260af3383f13d
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-02-15 18:35:40 -0600

    http: Crear el HTML para subida de archivos

--- src/index.js
+++ src/index.js
@@ -3,27 +3,26 @@
 
 //import { init } from './js/usuarios-page';
 
+import{ init } from './js/archivos-page';
+
+init();
 //obtenerUsuarios().then( console.log)
 
 //init();
 
-import * as CRUD from './js/crud-provider';
+// import * as CRUD from './js/crud-provider';
 
-CRUD.getUsuario(1).then(console.log);
+// CRUD.getUsuario(1).then(console.log);
 
-CRUD.createUsuario({
-    name: 'César',
-    job: 'Albañil'
-}).then(console.log);
+// CRUD.createUsuario({
+//     name: 'César',
+//     job: 'Albañil'
+// }).then(console.log);
 
-CRUD.actualizarUsuario(1, {
-    name: 'Verónica',
-    job: 'Comerciante'
-}).then(console.log);
+// CRUD.actualizarUsuario(1, {
+//     name: 'Verónica',
+//     job: 'Comerciante'
+// }).then(console.log);
 
 
-setTimeout(() => {
-
-    CRUD.borrarUsuario(1).then(console.log);
-
-}, 2000);
+// CRUD.borrarUsuario(1).then(console.log);
